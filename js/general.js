8a46ba8193df02b0c36a96ef6e9debd9ca48d803
jessicamrey <lamas676@hotmail.com>
2026-02-24 00:19:13 +0100

    -quotes update

--- js/general.js
+++ js/general.js
@@ -1,7 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
   const btn = document.getElementById('readMoreBtn');
   const hiddenContent = document.querySelector('.hidden-content');
+  
+  const quotes = document.querySelectorAll('.quote-content');
+  const prevBtn = document.querySelector('.quote-nav.left');
+  const nextBtn = document.querySelector('.quote-nav.right');
+  
+  //load quotes
+    fetch('quotes.html')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No se pudo cargar quotes.html');
+      }
+      return response.text();
+    })
+    .then(html => {
+      document.getElementById('quotes-container').innerHTML = html;
+      initQuotesCarousel();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+	
+  // carrousel
+function initQuotesCarousel() {
+  const quotes = document.querySelectorAll('.quote-content');
+  const prevBtn = document.querySelector('.quote-nav.left');
+  const nextBtn = document.querySelector('.quote-nav.right');
 
+  let currentIndex = 0;
+
+  function showQuote(index, direction) {
+    quotes.forEach(q => {
+      q.classList.remove('active', 'slide-in-left', 'slide-in-right');
+    });
+
+    const activeQuote = quotes[index];
+    activeQuote.classList.add('active');
+
+    if (direction === 'next') {
+      activeQuote.classList.add('slide-in-right');
+    } else if (direction === 'prev') {
+      activeQuote.classList.add('slide-in-left');
+    }
+  }
+
+  prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + quotes.length) % quotes.length;
+    showQuote(currentIndex, 'prev');
+  });
+
+  nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % quotes.length;
+    showQuote(currentIndex, 'next');
+  });
+
+  showQuote(currentIndex, 'next');
+}
+
+
+  //Intro Read more
   btn.addEventListener('click', function () {
     hiddenContent.classList.toggle('show');
 
@@ -9,6 +67,8 @@ document.addEventListener('DOMContentLoaded', function () {
       ? 'READ LESS'
       : 'READ MORE';
   });
+  
+  
 });
 
 
@@ -29,3 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
     btn.textContent = expanded ? 'Leer menos' : 'Leer más';
   });
 });*/
+
+
+  
+
